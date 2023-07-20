f901dfd9c7b3e51ee3acf99b2dc690e8a2b8bd76
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-07-20 22:52:39 +0530

    demo json data fetch and display

--- HTTP/index.js
+++ HTTP/index.js
@@ -1,13 +1,19 @@
 const http = require('http');
-const data = {
-    name: 'Rohit',
-    age: '22'
-}
+const fs = require('fs');
+// const data = {
+//     name: 'Rohit',
+//     age: '22'
+// }
+const index = fs.readFileSync('index.html','utf-8');
+const data = fs.readFileSync('data.json','utf-8')
 const server = http.createServer((req,res) => {
     console.log("server started");
-    res.setHeader('Dummy','Dummy value')
-    res.setHeader('Content-Type', 'application/json')
+    // res.setHeader('Dummy','Dummy value')
+    res.setHeader('Content-Type','application/json');
+    res.setHeader('Content-Type', 'text/html')
     // res.end('<h1>Hello, World!</h1>')
-    res.end(JSON.stringify(data));
+    // res.end(JSON.stringify(data)); 
+    // res.end(index)
+    res.end(data)
 })
 server.listen(8080);
