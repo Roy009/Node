7d7dde652aef5fc3a58b3e4baeda95cbfd07bfa9
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-07-22 23:09:32 +0530

    middleware and endpoints

--- Express/index.js
+++ Express/index.js
@@ -2,12 +2,43 @@ const fs = require('fs')
 const express = require('express')
 const server = express();
 const data = fs.readFileSync('data.json');
+
+//MIDDLEWARE
+server.use((req,res,next) => {
+    console.log(req.method, req.ip, req.hostname, req.get('User-Agent'));
+    next()
+})
+
+// API - ENDPOINT - ROUTE
 server.get('/', (req,res) => {
     // res.send('<h1>Hello, World From Node Js.</h1>')
     // res.sendFile('C:\\Users\\ROHIT ROY\\OneDrive\\Desktop\\Node\\Express\\index.html');
-    res.json(data)
+    res.json({type: 'GET'})
 })
-
+server.post('/',(req,res) => {
+    res.json({type: 'POST'})
+})
+server.put('/',(req,res) => {
+    res.json({type: 'PUT'})
+})
+server.delete('/',(req,res) => {
+    res.json({type: 'DELETE'})
+})
+server.patch('/',(req,res) => {
+    res.json({type: 'PATCH'})
+})
 server.listen(8080, () => {
     console.log("Server Started");
 });
+
+
+
+
+
+
+
+
+
+
+
+
