0dfbb69346e6b573149a4eb62e875d0f97800f4d
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-07-26 23:01:40 +0530

    express middleware

--- Express/index.js
+++ Express/index.js
@@ -4,20 +4,34 @@ const server = express();
 const data = fs.readFileSync('data.json');
 
 //MIDDLEWARE
+server.use(express.json())
 server.use((req,res,next) => {
     console.log(req.method, req.ip, req.hostname, req.get('User-Agent'));
     next()
 })
 
 // API - ENDPOINT - ROUTE
-server.get('/', (req,res) => {
+const auth = (req,res,next) => {
+    // console.log(req.query);
+    // if(req.query.password === '123'){
+    //     next()
+    // }else{
+    //     res.sendStatus(401);
+    // }
+    if(req.body.password === '123'){
+        next()
+    }else{
+        res.sendStatus(401);
+    }
+}
+server.get('/', auth ,(req,res) => {
     // res.send('<h1>Hello, World From Node Js.</h1>')
     // res.sendFile('C:\\Users\\ROHIT ROY\\OneDrive\\Desktop\\Node\\Express\\index.html');
     res.json({type: 'GET'})
 })
-// server.post('/',(req,res) => {
-//     res.json({type: 'POST'})
-// })
+server.post('/', auth, (req,res) => {
+    res.json({type: 'POST'})
+})
 // server.put('/',(req,res) => {
 //     res.json({type: 'PUT'})
 // })
@@ -27,15 +41,7 @@ server.get('/', (req,res) => {
 // server.patch('/',(req,res) => {
 //     res.json({type: 'PATCH'})
 // })
-const auth = (req,res,next) => {
-    console.log(req.query);
-    if(req.query.password === '123'){
-        next()
-    }else{
-        res.sendStatus(401);
-    }
-}
-server.use(auth)
+// server.use(auth)
 server.listen(8080, () => {
     console.log("Server Started");
 });
