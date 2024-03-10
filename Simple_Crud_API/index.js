c7cafe547e805c32300834ed04d07bcc89c17af3
Rohit009 <63160366+Roy009@users.noreply.github.com>
2024-03-10 22:31:49 +0530

    updated

--- Simple_Crud_API/index.js
+++ Simple_Crud_API/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
 
 mongoose
   .connect(
-    "mongodb+srv://haris2iftikhar:GClTzr15XhkjvN6k@backenddb.nrurtot.mongodb.net/Node-API?retryWrites=true&w=majority"
+    "mongodb+srv://admin:Hcl0908@backenddb.ubkp7oe.mongodb.net/?retryWrites=true&w=majority&appName=backendDB"
   )
   .then(() => {
     console.log("Connected to database!");
